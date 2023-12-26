e06bbb1946ebce0a8ed2da67b0be596404f1422b
Admin <kirill05035@gmail.com>
2023-12-26 22:00:11 +0300

    updated renaming multiple files

--- main.cpp
+++ main.cpp
@@ -12,20 +12,20 @@ int main(int argc, char** argv){
     fs::path p = fs::current_path() / argv[1];
     if(strcmp(argv[2], "-r") == 0){
         std::string old_name = argv[3];
-        std::string new_name = argv[4];
         std::regex reg(old_name);
         for(auto& x : fs::recursive_directory_iterator(p)){
             if(std::regex_search(x.path().filename().string(), reg)){
+                std::string new_name = std::regex_replace(x.path().filename().string(), reg, argv[4]);
                 fs::rename(x.path(), x.path().parent_path() / new_name);
             }
         }
     }
     else{
         std::string old_name = argv[2];
-        std::string new_name = argv[3];
         std::regex reg(old_name);
         for(auto& x : fs::directory_iterator(p)){
             if(std::regex_search(x.path().filename().string(), reg)){
+                std::string new_name = std::regex_replace(x.path().filename().string(), reg, argv[3]);
                 fs::rename(x.path(), x.path().parent_path() / new_name);
             }
         }
